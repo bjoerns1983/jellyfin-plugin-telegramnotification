b455b34147eefe2a482bd01b7c1fe5da1c39dead
bjoerns1983 <bjoern@sengotta.net>
2019-08-26 10:18:11 +0200

    Rename

--- Jellyfin.Plugin.TelegramNotification/Plugin.cs
+++ Jellyfin.Plugin.TelegramNotification/Plugin.cs
@@ -5,12 +5,20 @@ using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
+using System.IO;
 
 namespace Jellyfin.Plugin.TelegramNotification
 {
     public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
-        public override string Name => "Telegram Notifications";
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+        }
+        public override string Name
+        {
+            get { return "Telegram Notifications"; }
+        }
 
         public override string Description
         {
@@ -21,10 +29,7 @@ namespace Jellyfin.Plugin.TelegramNotification
         }
 
         public override Guid Id => Guid.Parse("37eab764-2033-4c03-8ba0-651d19c08c72");
-        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
-        {
-            Instance = this;
-        }
+
 
         public static Plugin Instance { get; private set; }
 
@@ -34,8 +39,8 @@ namespace Jellyfin.Plugin.TelegramNotification
             {
                 new PluginPageInfo
                 {
-                    Name = this.Name,
-                    EmbeddedResourcePath = string.Format("{0}.Configuration.configPage.html", GetType().Namespace)
+                    Name = Name,
+                    EmbeddedResourcePath = GetType().Namespace + ".Configuration.config.html"
                 }
             };
         }
